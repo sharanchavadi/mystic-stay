b57c446a4c8bc1e94a95dad5840d6bfb138f2e03
sharanchavadi <sharanschavadi@gmail.com>
2020-06-01 16:50:07 +0530

    Implemented CRUD methods for abastract class+routes for properties

--- controllers/abstract.js
+++ controllers/abstract.js
@@ -11,4 +11,35 @@ module.exports = class AbstractClass{
             .then(result => res.send(result));
     }
 
+    all(req, res) {
+        return this.model.find({})
+            .then(result => res.send(result));
+    }
+
+    read(req, res) {
+        return this.model.findById(req.params.id)
+            .then(result => {res.send(result)});
+    }
+
+    delete(req, res) {
+        return this.model.findByIdAndRemove(req.params.id)
+            .then(result => res.send(result));
+    }
+
+    _update(id, data) {
+        return this.model.findByIdAndUpdate(id, {$set: data})
+            .then(result => {
+                    return this.model.findById(id).then(object => {
+                        return object;
+                    })
+                })
+    }
+
+    update(req, res) {
+        //console.log(req);
+        return this._update(req.params.id, req.body).then(updatedObject => {
+            res.send(updatedObject);
+        });
+    }
+
 }
--- routes/property.route.js
+++ routes/property.route.js
@@ -7,5 +7,9 @@ const PropertiesController = require('../controllers/properties.controller');
 let propertyController = new PropertiesController();
 
 router.post('/', (...args) => propertyController.create(...args));
+router.get('/:id', (...args) => propertyController.read(...args));
+router.put('/:id', (...args) => propertyController.update(...args));
+router.delete('/:id', (...args) => propertyController.delete(...args));
+router.get('/', (...args) => propertyController.all(...args));
 
 module.exports = router;
